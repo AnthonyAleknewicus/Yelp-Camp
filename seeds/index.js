1f1c0f24d7dfdcc99ac72ad6a3f68e98ca457766
AnthonyAleknewicus <anthonyaleknewicus@gmail.com>
2024-04-29 21:20:48 -0700

    basic bootstrap styling added to index, new, edit, and show pages

--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground ({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, accusantium nihil quibusdam quaerat sequi atque suscipit voluptatum beatae ea quae consequatur ullam fugiat libero mollitia eos molestias blanditiis nemo fuga! Amet, eius aut. Dicta optio nisi dolores molestiae maiores eveniet eos nihil quae amet, aperiam suscipit similique fugiat minus totam illo. Odit.",
+            price
         })
         await camp.save();
     }
