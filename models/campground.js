86d29d6f575ec99e9c272686352c0b2d63fd67a8
AnthonyAleknewicus <anthonyaleknewicus@gmail.com>
2024-05-24 15:59:48 -0700

    added deletion of reviews individually or delete middleware by deleting camp

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { campgroundSchema } = require('../schemas');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -13,6 +15,16 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 });
 
 //exports the basic model Campground from the CampgroundSchema
