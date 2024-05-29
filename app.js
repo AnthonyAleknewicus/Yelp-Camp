fd4cff003b2d930349cff494f04e32baf183058b
AnthonyAleknewicus <anthonyaleknewicus@gmail.com>
2024-05-29 15:01:29 -0700

    added session and flash

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const engine = require('ejs-mate');
 
 const Campground = require('./models/campground');
 const Review = require('./models/review');
-const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema, reviewSchema } = require('./schemas')
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -29,6 +29,27 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false, 
+    saveUninitialized: true,
+    cookie: { 
+        httpOnly: true,
+        secure: false,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7        
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next()
+})
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,43 +19,60 @@ const validateCampground = (req, res, next) => {
     }
 }
 
-
+// index of all campgrounds
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 }))
 
+// form to create a new campground
 router.get('/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
+// post request creating a new campground
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError(400, "Invalid Campground Data");    
     const campground = new Campground(req.body.campground);
     await campground.save()
+    req.flash('success', 'Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
+// show route
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
     // console.log(campground)
-    res.render('campgrounds/show', { campground });
+    if(!campground) {
+        req.flash('error', 'Cannot find that campground!')
+        res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground } );
 }))
 
+// form to edit a campground
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground) {
+        req.flash('error', 'Cannot find that campground!')
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
 }))
 
+// put request to edit/update the campground
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
+// delete request to delete the campground
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground!')
     res.redirect('/campgrounds');
 }))
 
