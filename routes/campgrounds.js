2a594bd8a4b32dc26ff0311dafa1962944dae7c5
AnthonyAleknewicus <anthonyaleknewicus@gmail.com>
2024-06-24 11:21:56 -0700

    added reviews permissions and authorizations

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -34,7 +34,13 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 // show route
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    const campground = await Campground.findById(req.params.id).populate({ 
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        } 
+    }).populate('author');
+    // console.log(campground)
     if(!campground) {
         req.flash('error', 'Cannot find that campground!')
         res.redirect('/campgrounds');
