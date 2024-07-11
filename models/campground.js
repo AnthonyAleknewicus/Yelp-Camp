7fbcbc000dc58f06d08479a02d5ac23e20e745e1
AnthonyAleknewicus <anthonyaleknewicus@gmail.com>
2024-07-11 13:19:28 -0700

    applied geocoding and geoJSON to campground location

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,31 @@ const { campgroundSchema } = require('../schemas');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/dlarslo8z/image/upload/v1720284452/YelpCamp/ck3xgzp4iubcsw8vrzow.jpg
+
+const ImageSchema = new Schema({
+        url: String,
+        filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/h_100,w_200');
+})
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
+    images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
         }
-    ],
+    },
     price: Number,
     description: String,
     location: String,
